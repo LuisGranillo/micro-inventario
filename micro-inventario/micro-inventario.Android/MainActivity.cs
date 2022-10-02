7fc17f4dc641808817b1c73aec2a1beba4c2ae83
LuisGranillo <79174029+LuisGranillo@users.noreply.github.com>
2022-10-01 21:14:38 -0500

    update3

--- micro-inventario/micro-inventario.Android/MainActivity.cs
+++ micro-inventario/micro-inventario.Android/MainActivity.cs
@@ -4,6 +4,8 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Xamd.ImageCarousel.Forms.Plugin.Droid;
+using CarouselView.FormsPlugin.Droid;
 
 namespace micro_inventario.Droid
 {
@@ -17,6 +19,8 @@ namespace micro_inventario.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+            ImageCarouselRenderer.Init(); //carrusel
+            CarouselViewRenderer.Init(); //Carrusel automático
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
